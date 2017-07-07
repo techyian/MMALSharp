759d954c434473ef673e9925587b400baa58f05d
techyian <ian@techyian.co.uk>
2017-07-07 07:55:48 +0100

    Moving to new dotnet build system. Removed paket and FAKE (may re-add in future if necessary). .NET Core build not currently working!

--- build.cmd
+++ build.cmd
@@ -1,31 +1,5 @@
 @echo off
 cls
 
-SET ROOT=%CD%
-SET PWD=%CD%\build.fsx
-
-echo %PWD%
-
-cd .paket
-paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-paket.exe update
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-cd ..
-
-packages\netframework\FAKE\tools\FAKE.exe "%PWD%" MMALSharpReleaseApp
-packages\netframework\FAKE\tools\FAKE.exe "%PWD%" MMALSharpDebugApp
-
-packages\netframework\FAKE\tools\FAKE.exe "%PWD%" MMALSharpCommonReleaseApp
-packages\netframework\FAKE\tools\FAKE.exe "%PWD%" MMALSharpCommonDebugApp
-
-packages\netframework\FAKE\tools\FAKE.exe "%PWD%" MMALSharpFFmpegReleaseApp
-packages\netframework\FAKE\tools\FAKE.exe "%PWD%" MMALSharpFFmpegDebugApp
-
-.paket\paket.exe pack output build templatefile src\MMALSharp\paket.template
+dotnet restore
+dotnet build --framework net452
