67804357fd5471b846d995d350ce713721fa5d98
Jon McGuire <mcguirev10@gmail.com>
2020-08-04 15:24:26 -0400

    fix copyright filenames

--- src/MMALSharp.Processing/Handlers/OnDemandImageCaptureHandler.cs
+++ src/MMALSharp.Processing/Handlers/OnDemandImageCaptureHandler.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ImageStreamCaptureHandler.cs" company="Techyian">
+﻿// <copyright file="OnDemandImageCaptureHandler.cs" company="Techyian">
 // Copyright (c) Ian Auty and contributors. All rights reserved.
 // Licensed under the MIT License. Please see LICENSE.txt for License info.
 // </copyright>
